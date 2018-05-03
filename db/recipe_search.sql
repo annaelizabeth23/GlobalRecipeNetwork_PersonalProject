cb8f09f7b9425eee7d8ca63bf33da293c438e975
Anna Bender <annaelizabeth23@gmail.com>
2018-05-02 21:05:53 -0700

    fixed bug with recipe searching

--- db/recipe_search.sql
+++ db/recipe_search.sql
@@ -1,3 +1,3 @@
-SELECT recipes.title, users_auth0_mini.name FROM recipes
-FULL JOIN users_auth0_mini ON users_auth0_mini.auth0_id = recipes.auth0_id
+SELECT * FROM recipes
+FULL OUTER JOIN users_auth0_mini ON users_auth0_mini.auth0_id = recipes.auth0_id
 WHERE recipes.title LIKE '%$1:value%';
